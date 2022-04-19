174ad71cc89980fd1a5534343bf16862667c456c
Trusan ACAR <98050089+trsnacar@users.noreply.github.com>
2022-04-19 10:18:05 +0300

    BlockchainInGolang

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+)
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+type BlockChain struct {
+	blocks []*Block
+}
+
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+
+	block.DeriveHash()
+	return block
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, new)
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockChain() *BlockChain {
+	return &BlockChain{[]*Block{Genesis()}}
+}
+
+func main() {
+	chain := InitBlockChain()
+	chain.AddBlock("Genesis'ten sonraki ilk BLOK")
+	chain.AddBlock("Genesis'ten sonra ikinci BLOK")
+	chain.AddBlock("Genesis'ten sonra üçüncü BLOK")
+
+	for _, block := range chain.blocks {
+		fmt.Printf("Previous hash: %x\n", block.PrevHash)
+		fmt.Printf("Data hash: %s\n", block.Data)
+		fmt.Printf(" hash: %x\n", block.Hash)
+	}
+}
